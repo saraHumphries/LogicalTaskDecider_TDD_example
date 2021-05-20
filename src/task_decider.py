31a203d2f58a64009accc628ac3eabb7d54a4fb7
sara <smhumphries@hotmail.co.uk>
2021-05-20 13:22:56 +0100

    sets up get_preferred_option function

---
+++ src/task_decider.py
@@ -0,0 +1,2 @@
+def get_preferred_option(task1, task2):
+    return "clean windows"
--- tests/task_test.py
+++ tests/task_test.py
@@ -1,5 +1,7 @@
 from src.task import Task
 import unittest
+from src.task_decider import get_preferred_option
+
 
 class TestTask(unittest.TestCase):
 
@@ -13,3 +15,11 @@ class TestTask(unittest.TestCase):
 
     def test_task_has_duration(self):
         self.assertEqual(20, self.task_clean_windows.duration)
+
+
+    def test_clean_window_chosen_over_wash_dishes(self):
+        self.assertEqual("clean windows", get_preferred_option("clean windows", "wash dishes"))
+
+    
+
+    
